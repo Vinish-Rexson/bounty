ec3e9377bd001ecbad49cb48d0fae0ddd5ae9830
NISHANTTMAURYA <mauryanishant2005@gmail.com>
2025-01-11 16:40:02 +0530

    hur

--- project/urls.py
+++ project/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', views.home, name='home'),
+    path('', RedirectView.as_view(pattern_name='dev:dashboard')),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('signup/', views.signup_view, name='signup'),
